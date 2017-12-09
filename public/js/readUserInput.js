6f92dbe6b324657d9bcd8b5e947248d1b44b09f5
cchara11 <04061991Cc#>
2017-12-09 17:23:54 +0100

    final

--- public/js/readUserInput.js
+++ public/js/readUserInput.js
@@ -1,27 +1,15 @@
 function validate(nextPage){
     $(function($http){
         var radioData = document.getElementsByTagName("input");
-        // for (i = 0; i < radioData.length; i+=2)
-        // {
-        //     if (radioData[i].checked || radioData[i+1].checked)
-        //     {
-        //         continue;
-        //     }
-        //     alert("Please fill an answer for scenario " + radioData[i].value);
-        //     return;
-        // }
-
-        var data = {
-            answers:[]
-        };
-        for (i = 0; i < radioData.length; i++)
+        for (i = 0; i < radioData.length; i+=2)
         {
-            data.answers.push({
-                "videoName" : radioData[i].id, 
-                "answer" : radioData[i].checked,
-                "scenario" : radioData[i].value
-            });
-        } 
+            if (radioData[i].checked || radioData[i+1].checked)
+            {
+                continue;
+            }
+            alert("Please fill an answer for scenario " + radioData[i].value);
+            return;
+        }
 
         var data2 = {
             answers2:[]
